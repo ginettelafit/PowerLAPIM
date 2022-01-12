fca4a8f06839da1663190b21d2e8deadbe7cfff0
Ginette Lafit <ginettelafit19@gmail.com>
2022-01-12 14:05:03 +0100

    Update Performance.Dyad.Model.6.R
    Change optimizer.

--- R/Performance.Dyad.Model.6.R
+++ R/Performance.Dyad.Model.6.R
@@ -45,8 +45,9 @@ fit.Model.6 = lme(fixed = Y ~ 1 + X.Actor + X.Partner + W +
                   weights = varIdent(form = ~1|Gender),
                   data = data, na.action=na.omit, 
                   method = 'REML',
-                  control = lmeControl(optimizer="bobyqa", optCtrl=list(maxfun=2e5), 
-                  maxIter=500, msMaxIter=500, msVerbose = FALSE))  
+                  control = lmeControl(opt = "optim", method = "L-BFGS-B",
+                  optCtrl=list(maxfun=2e5),msVerbose=FALSE,         
+                  maxIter=1000,msMaxIter=1000)) 
 }
 
 if (is.REML==FALSE){
@@ -57,8 +58,9 @@ fit.Model.6 = lme(fixed = Y ~ 1 + X.Actor + X.Partner + W +
                   weights = varIdent(form = ~1|Gender),
                   data = data, na.action=na.omit, 
                   method = 'ML',
-                  control = lmeControl(optimizer="bobyqa", optCtrl=list(maxfun=2e5), 
-                  maxIter=500, msMaxIter=500, msVerbose = FALSE))  
+                  control = lmeControl(opt = "optim", method = "L-BFGS-B",
+                  optCtrl=list(maxfun=2e5),msVerbose=FALSE,         
+                  maxIter=1000,msMaxIter=1000)) 
 }
 
 # Performance measures 
