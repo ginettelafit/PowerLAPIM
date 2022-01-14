d9735a6270423297ebc95b085d4de3f94686e8cf
Ginette Lafit <ginettelafit19@gmail.com>
2022-01-14 17:32:42 +0100

    Update Performance.Dyad.Model.11.R

--- R/Performance.Dyad.Model.11.R
+++ R/Performance.Dyad.Model.11.R
@@ -51,9 +51,7 @@ fit.Model.11 = lme(fixed = Y ~ -1 + Female + Female.Z + Female:X.Actor +
                   weights = varIdent(form = ~1|Gender),
                   data = data, na.action=na.omit, 
                   method = 'REML',
-                  control = lmeControl(opt = "optim", method = "L-BFGS-B",
-                  optCtrl=list(maxfun=2e5),msVerbose=FALSE,         
-                  maxIter=1000,msMaxIter=1000)) 
+                  control=list(msVerbose=TRUE, maxIter=500, msMaxIter=500))
 }
 
 
@@ -69,9 +67,7 @@ fit.Model.11 = lme(fixed = Y ~ -1 + Female + Female.Z + Female:X.Actor +
                   weights = varIdent(form = ~1|Gender),
                   data = data, na.action=na.omit, 
                   method = 'ML',
-                  control = lmeControl(opt = "optim", method = "L-BFGS-B",
-                  optCtrl=list(maxfun=2e5),msVerbose=FALSE,         
-                  maxIter=1000,msMaxIter=1000)) 
+                  control=list(msVerbose=TRUE, maxIter=500, msMaxIter=500))
 }
 
 # Performance measures 
