1d070ca4e33b9c517e302ddb376f43082d155392
Ginette Lafit <ginettelafit19@gmail.com>
2022-01-14 17:40:20 +0100

    Update Performance.Dyad.Model.15.lag.R

--- R/Performance.Dyad.Model.15.lag.R
+++ R/Performance.Dyad.Model.15.lag.R
@@ -54,9 +54,7 @@ fit.Model.15 = lme(fixed = Y ~ -1 + Female + Female:Y.lag +
                   weights = varIdent(form = ~1|Gender),
                   data = data, na.action=na.omit, 
                   method = 'REML',
-                  control = lmeControl(opt = "optim", method = "L-BFGS-B",
-                  optCtrl=list(maxfun=2e5),msVerbose=FALSE,         
-                  maxIter=1000,msMaxIter=1000)) 
+                  control=list(msVerbose=TRUE, maxIter=500, msMaxIter=500))
 }
 
 if (is.REML==FALSE){
@@ -75,9 +73,7 @@ fit.Model.15 = lme(fixed = Y ~ -1 + Female + Female:Y.lag +
                   weights = varIdent(form = ~1|Gender),
                   data = data, na.action=na.omit, 
                   method = 'ML',
-                  control = lmeControl(opt = "optim", method = "L-BFGS-B",
-                  optCtrl=list(maxfun=2e5),msVerbose=FALSE,         
-                  maxIter=1000,msMaxIter=1000)) 
+                  control=list(msVerbose=TRUE, maxIter=500, msMaxIter=500))
 }
 
 # Performance measures 
